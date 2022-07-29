65392fb8c4bd639b47800a39621394349ad3e7b5
korodoff <rudoffsutnga368@gmail.com>
2022-07-29 14:11:53 +0530

    adding cors to provide authentication

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 const express = require ("express");
 const app = express()
+const cors = require("cors")
  const dataRouter = require("./routes/data")
 // app.get("/home", function(req,res){
 //     res.send("home")
 // })
 
-
+app.use(cors())
 app.use("/api", dataRouter)
 
 app.listen(process.env.PORT||8080, function(){
